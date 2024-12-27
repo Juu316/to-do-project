c3d69b97c9a6a43f2b1a821f8eee7c80add6ae87
Juu316 <uujuu316@gmail.com>
2024-12-27 12:48:07 +0800

    daily

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const div = document.createElement("div");
+const body = document.body;
+const elementw = document.getElementsByClassName("heading");
+elementw.onclick = function () {
+  console.log("hello");
+  //   document.getElementById(form1).innerHTML = "<div>hello</div>";
+  //   document.getElementById(form1)=document.getElementById(form1);
+};
+elementw.addEventListener("click", function () {
+  console.log("hello");
+});
+/*
 
+
+ / /  \ \  
+/ /    \ \ 
+\ \    / / 
+ \_\  /_/
+
+ 
+ */
